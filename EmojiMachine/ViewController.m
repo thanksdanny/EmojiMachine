c5f337df5a9ab0a557509844fcd8ec4d81a972ab
thanksdanny <hj-07@163.com>
2016-03-08 11:17:57 +0800

    搞定表情的展示，昨晚一直被坑的问题，原来是没有把number转回来

--- EmojiMachine/ViewController.m
+++ EmojiMachine/ViewController.m
@@ -8,20 +8,98 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UIPickerView *emojiPickerView;
+@property (weak, nonatomic) IBOutlet UIButton *goBtn;
+@property (weak, nonatomic) IBOutlet UILabel *bingoLabel;
+
+@property (nonatomic, strong) NSArray *imageArray;
+@property (nonatomic, strong) NSMutableArray *dataArray1;
+@property (nonatomic, strong) NSMutableArray *dataArray2;
+@property (nonatomic, strong) NSMutableArray *dataArray3;
+
 
 @end
 
 @implementation ViewController
-
+#pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.imageArray = @[@"👻",@"👸",@"💩",@"😘",@"🍔",@"🤖",@"🍟",@"🐼",@"🚖",@"🐷"];
+
+    self.emojiPickerView.delegate = self;
+    self.emojiPickerView.dataSource = self;
+    
+    self.dataArray1 = [[NSMutableArray alloc] init];
+    self.dataArray2 = [[NSMutableArray alloc] init];
+    self.dataArray3 = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < 100; i++) {
+        [self.dataArray1 addObject:@(arc4random() % 10)];
+        [self.dataArray2 addObject:@(arc4random() % 10)];
+        [self.dataArray3 addObject:@(arc4random() % 10)];
+    }
+    
+    self.goBtn.layer.cornerRadius = 6;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+#pragma mark - goAction
+
+- (IBAction)goBtnDidTouch {
+    
+}
+
+
+
+#pragma mark - pickerDelegate & dataSource
+
+// 每列个数
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return 100;
+}
+
+// 列数
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 3;
+}
+
+// 每列宽度
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
+    return 100.0;
+}
+
+//
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component {
+    return 100.0;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
+    UILabel *pickerLabel = [[UILabel alloc] init];
+    if (component == 0) {
+        pickerLabel.text = self.imageArray[[self.dataArray1[row] integerValue]];
+//        pickerLabel.text = self.imageArray[(int)self.dataArray3[row]]; // 错误写法 应该number转回数字，之前一直被这里坑了
+    } else if (component == 1) {
+        pickerLabel.text = self.imageArray[[self.dataArray2[row] integerValue]];
+    } else {
+        pickerLabel.text = self.imageArray[[self.dataArray3[row] integerValue]];
+    }
+    
+    pickerLabel.font = [UIFont fontWithName:@"Apple Color Emoji" size:80];
+    pickerLabel.textAlignment = NSTextAlignmentCenter;
+    
+    return pickerLabel;
+}
+
 
 @end
